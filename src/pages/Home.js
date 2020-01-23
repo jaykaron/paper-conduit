c8252c1fd35b9a84ef2f27e774543faffe0213e2
jaykaron <jaykaron@gmail.com>
2020-01-23 15:26:27 -0500

    Remove Splash on Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,19 @@
 import React from 'react'
-import Splash from '../components/Splash'
 import ArticleList from '../components/ArticleList'
 import TagModal from '../components/TagModal'
 import { QueryProvider } from '../components/QueryContext'
 import FeedButton from '../components/buttons/FeedButton'
 
-const Home = (props) => {
-
-  const content = (
-    <p>
-      A React app using the RealWorld API with a custom PaperCSS frontend.
-    </p>
-  )
-
-  return (
-    <div>
-      <Splash title='Paper Conduit'>
-        {content}
-      </Splash>
-      <QueryProvider>
-        <div className="article-card-buttons">
-          <FeedButton />
-          <TagModal />
-        </div>
-        <ArticleList />
-      </QueryProvider>
-    </div>
-  )
-}
+const Home = () => (
+  <div>
+    <QueryProvider>
+      <div className="article-card-buttons">
+        <FeedButton />
+        <TagModal />
+      </div>
+      <ArticleList />
+    </QueryProvider>
+  </div>
+)
 
 export default Home
