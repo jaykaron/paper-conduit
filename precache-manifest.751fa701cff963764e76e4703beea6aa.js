1e1c6b2fefa8101bb757d486a6ece3cf1be699eb
jaykaron <jaykaron@gmail.com>
2020-01-26 23:01:59 -0500

    Updates

---
+++ precache-manifest.751fa701cff963764e76e4703beea6aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2801e1af3a01d0c558f24d63a1275238",
+    "url": "/paper-conduit/index.html"
+  },
+  {
+    "revision": "c3adef5070f1dfd542c3",
+    "url": "/paper-conduit/static/css/main.8f17c67b.chunk.css"
+  },
+  {
+    "revision": "28868c8e49557b7bc4ec",
+    "url": "/paper-conduit/static/js/2.c18712f9.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/paper-conduit/static/js/2.c18712f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c3adef5070f1dfd542c3",
+    "url": "/paper-conduit/static/js/main.4bdf4d3d.chunk.js"
+  },
+  {
+    "revision": "dfbc2ac176be47faf8e7",
+    "url": "/paper-conduit/static/js/runtime-main.d8cd0cf4.js"
+  }
+]);
